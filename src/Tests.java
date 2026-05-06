62e343f56638062cf32c71bb0d439eaafa2efe58
gustavocorreia0 <gustavoCorreiaa0@gmail.com>
2026-05-05 22:27:27 -0300

    Teste dos casos negados e conferência de ordem

--- src/Tests.java
+++ src/Tests.java
@@ -4,12 +4,16 @@ import service.TransactionService;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
 
 public class Tests {
 
     public static void main(String[] args) {
 
-        checkRealizedTransaction();
+        checkApprovedTransaction();
+        checkDisapprovedTransaction();
+        checkDatetimeOrder();
 
         System.out.println("Todos os testes foram concluídos!");
 
@@ -21,7 +25,7 @@ public class Tests {
         }
     }
 
-    private static void checkRealizedTransaction() {
+    private static void checkApprovedTransaction() {
         ClientRepository repository = new ClientRepository();
         TransactionService service = new TransactionService(repository);
 
@@ -32,6 +36,30 @@ public class Tests {
         assertEquals(new BigDecimal("150"), repository.findClient("2147483649").getSaldo());
     }
 
+    private static void checkDisapprovedTransaction() {
+        ClientRepository repository = new ClientRepository();
+        TransactionService service = new TransactionService(repository);
 
+        Transaction transaction = new Transaction(4, LocalDateTime.parse("2023-09-09T14:17:00"),"675869708","210385733", new BigDecimal("5300"));
+        service.postTransaction(transaction);
+
+        assertEquals(new BigDecimal("4900"), repository.findClient("675869708").getSaldo());
+        assertEquals(new BigDecimal("10"), repository.findClient("210385733").getSaldo());
+    }
+
+    private static void checkDatetimeOrder() {
+        ClientRepository repository = new ClientRepository();
+        TransactionService service = new TransactionService(repository);
+
+        List<Transaction> transactions = List.of(
+                new Transaction(2, LocalDateTime.parse("2023-09-09T14:15:05"), "2147483649", "210385733", new BigDecimal("149")),
+                new Transaction(1, LocalDateTime.parse("2023-09-09T14:15:00"), "938485762", "2147483649", new BigDecimal("150"))
+        );
+        transactions.stream().sorted(Comparator.comparing(Transaction::getDatetime)).forEach(service::postTransaction);
+
+        assertEquals(new BigDecimal("30"), repository.findClient("938485762").getSaldo());
+        assertEquals(new BigDecimal("1"), repository.findClient("2147483649").getSaldo());
+        assertEquals(new BigDecimal("159"), repository.findClient("210385733").getSaldo());
+    }
 
 }
